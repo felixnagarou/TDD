3d39275c0748554f140c8fa470d78afe28daed47
HippolyteÃ <hippolyteaubin@gmail.com>
2023-08-24 14:47:45 +0200

    Ajout méthodes test

--- src/test/java/org/example/Exercice003/YearTest.java
+++ src/test/java/org/example/Exercice003/YearTest.java
@@ -3,11 +3,33 @@ package org.example.Exercice003;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
 public class YearTest {
+    final static List<Long> TEST_VALUES = Arrays.asList(32L, 2000L, 20000L); ;
     @Test
-    public void TestIsLeapYear(){
+    public void TestIsYearLeap1() {
         Year testYear = new Year();
-        final long TEST_VALUE = 2000;
-        Assertions.assertEquals(true, testYear.IsYearLeap(TEST_VALUE));
+        Assertions.assertEquals(true, testYear.IsYearLeap(TEST_VALUES.get(0)));
+
     }
+
+    @Test
+    public void TestIsLeapYear2() {
+        Year testYear = new Year();
+        Assertions.assertEquals(true, testYear.IsYearLeap(TEST_VALUES.get(1)));
+    }
+
+
+    @Test
+    public void TestIsLeapYear3() {
+        Year testYear = new Year();
+
+        Assertions.assertEquals(true, testYear.IsYearLeap(TEST_VALUES.get(2)));
+    }
+
 }
+
+
